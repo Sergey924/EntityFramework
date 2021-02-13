2509e5b5c6890817610e74cb22fd5ac41a8434d1
Sergey924 <74462379+Sergey924@users.noreply.github.com>
2021-02-13 22:56:27 +0200

    Add files via upload

---
+++ 1_to_M/Model1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace _1_to_M
+{
+    public class Model1 : DbContext
+    {
+
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+
+
+}
